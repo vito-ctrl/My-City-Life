ca1934d212d701331ff0e82b3cde8280fa79a830
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-02-13 11:53:39 +0100

    diagram de class

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import Moroccan_Architecture from '../assets/Moroccan Architecture.pnj'
+
+function Home () {
+    
+    return(
+        <div className="bg-gray-950 h-200 w-auto">
+            <div className="">
+                <h5 className="text-xs text-gray-500">OUR MISSION</h5>
+                <h1 className="text-white text-6xl font-bold">DISCOVER <br /> WHAT'S <br />HAPPENING</h1>
+                <h5 className="text-gray-500">Connect with your city's pulse. Whether you're exploring as a visitor <br /> or rediscovering as a local, MyCityLife brings you authentic <br />experiences.</h5>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+
+}
+
+export default Home;
