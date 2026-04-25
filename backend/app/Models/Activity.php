0baf0ced1aeea5283f8f1bc03a479f6f4dd7034b
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-04-25 16:16:09 +0100

    chore(models): enable factories for activity

--- backend/app/Models/Activity.php
+++ backend/app/Models/Activity.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Review;
@@ -9,6 +10,8 @@ use App\Models\Favorites;
 
 class Activity extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'title',
