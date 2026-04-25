3b73de0d9b684f91ec237a1535b398d89cca8203
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-04-25 16:16:10 +0100

    chore(models): enable factories for favorites

--- backend/app/Models/Favorites.php
+++ backend/app/Models/Favorites.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 use App\Models\Activity;
 
 class Favorites extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'activity_id',
