935bc13d8d5832cfd7e457f99f7363765b6c6c74
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-04-25 16:16:09 +0100

    chore(models): enable factories for user profile

--- backend/app/Models/UserProfile.php
+++ backend/app/Models/UserProfile.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserProfile extends Model
 {
+    use HasFactory;
 
     protected $fillable = [
         'user_id',
