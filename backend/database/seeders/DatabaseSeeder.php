45ee660405a9722b30175e76649366bb9d732ece
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-04-25 16:16:12 +0100

    chore(seeders): register seeding pipeline

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,20 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $this->call([
             RoleSeeder::class,
+            UserSeeder::class,
             ActivitySeeder::class,
             BusinessSeeder::class,
+            BookingSeeder::class,
+            ReservationSeeder::class,
+            EngagementSeeder::class,
+            NotificationSeeder::class,
         ]);
     }
 }
